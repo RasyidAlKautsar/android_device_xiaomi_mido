77ddcbacb66e0e6a5ea713f9348c34573881f32d
RasyidAlKautsar <arkan1986muhammad@gmail.com>
2022-03-05 13:35:55 +0700

    mido: Update Stuff Cherish
    Make build vanilla (test)

--- cherish_mido.mk
+++ cherish_mido.mk
@@ -26,12 +26,9 @@ $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
