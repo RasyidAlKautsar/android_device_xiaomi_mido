357e3c0d49a036bb4ef4d71a476efc927bfd126c
RasyidAlKautsar <arkan1986muhammad@gmail.com>
2022-03-06 06:05:03 +0700

    Vanilla!

--- cherish_mido.mk
+++ cherish_mido.mk
@@ -29,6 +29,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+CHERISH_VANILLA := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
