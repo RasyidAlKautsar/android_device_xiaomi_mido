55e34572a3b549503f914fd0abf01b4840a3003a
Mochammad-Reyhan-AF <96855112+Mochammad-Reyhan-AF@users.noreply.github.com>
2022-03-27 14:32:25 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mido.mk
+    $(LOCAL_DIR)/p_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mido-user \
-    aosp_mido-userdebug \
-    aosp_mido-eng
+    p_mido-user \
+    p_mido-userdebug \
+    p_mido-eng
