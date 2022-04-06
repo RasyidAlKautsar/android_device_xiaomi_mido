06f9980431eef6f5ebc400310ee7b9b7f4efca2f
RasyidAlKautsar <arkan1986muhammad@gmail.com>
2022-04-06 11:57:33 +0000

    mido: Vanilla Cherish
    Signed-off-by: RasyidAlKautsar <arkan1986muhammad@gmail.com>

--- cherish_mido.mk
+++ cherish_mido.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common CherishOS stuff.
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+CHERISH_VANILLA := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
